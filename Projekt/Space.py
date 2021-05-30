8f09eb994c99cace703083a6384b07d976ed0f69
amotka <80823825+amotka@users.noreply.github.com>
2021-05-31 00:34:14 +0100

    Update Space.py
    dodanie komentarzy; naprawienie błędu z wizualizacją stężeń; skrócenie metody Motion - stworzenie osobnych metod do wizualizacji: tła, stężenia wirusa i ludzi; uwzględnienie inwalls w rozprzestrzenianiu się wirusa; wirus "odbija się" od ścian

--- Projekt/Space.py
+++ Projekt/Space.py
@@ -17,6 +17,8 @@ class Object():
         self.__cashdesks = []
         self.__cashdesksinfX = []
         self.__cashdesksinfY = []
+        self.__inwalls=[]
+        self.__indoors=[]
         self.__humans = []
         self.__actives = []
         self.__wejscie = []
@@ -24,11 +26,8 @@ class Object():
         self.__grid = []
         self.__size = []
 
-    def __str__(self):
-        return('sciany: ' + str(self.__object) + '\ndrzwi: ' + str(self.__doors) + '\nwyłączone miejsca: ' + str(self.__excludeds) + '\nkasy: ' + str(self.__cashdesks))     
-
     def DrawPolygon(self,vertices,type_):
-        #zaznaczenie konturów w gridzie
+        #zaznaczenie danych punktów w Gridzie, tj. gdzie znajdują się ściany, regały, recepcja itp.
         for i in range (len(vertices)):
             x = [vertices[i%len(vertices)][0],vertices[(i+1)%len(vertices)][0]]
             y = [vertices[i%len(vertices)][1],vertices[(i+1)%len(vertices)][1]]
@@ -70,6 +69,8 @@ class Object():
             temp_cashdesksinfX = []
             temp_cashdesksinfY = []
             temp_active = []
+            temp_inwalls=[]
+            temp_indoors=[]
             line = line.split(' ')
             for i in range(int((len(line)-1))):
                 line_temp = line[i+1].split(',')
@@ -98,6 +99,10 @@ class Object():
                     self.__size.append(line_temp[1])
                 elif line[0] == 'active':
                     temp_active.append([line_temp[0],line_temp[1]])
+                elif line[0] == 'inwalls':
+                    temp_inwalls.append([line_temp[0],line_temp[1]])
+                elif line[0] == 'indoors':
+                    temp_indoors.append([line_temp[0],line_temp[1]])
             if line[0] == 'excluded':
                 self.__excludeds.append(temp_excluded)
             elif line[0] == 'door':
@@ -108,6 +113,10 @@ class Object():
                 self.__cashdesksinfY.append(temp_cashdesksinfY)
             elif line[0] == 'active':
                 self.__actives.append(temp_active)
+            elif line[0] == 'inwalls':
+                self.__inwalls.append(temp_inwalls) 
+            elif line[0] == 'indoors':
+                self.__indoors.append(temp_indoors) 
         for i in  range (self.__size[0]+1):
             line_temp = []
             for j in range (self.__size[1]+1):
@@ -124,26 +133,16 @@ class Object():
                 self.__cashdesksinfY[i] = max(self.__cashdesksinfY[i]) + 1
             elif cashdesksinf0[i] == 'above':
                 self.__cashdesksinfY[i] = min(self.__cashdesksinfY[i]) - 1
-            self.__cashdesksinfX[i] = [min(self.__cashdesksinfX[i]),max(self.__cashdesksinfX[i])]    
+            self.__cashdesksinfX[i] = [min(self.__cashdesksinfX[i]),max(self.__cashdesksinfX[i])]
+        for iw in self.__inwalls:
+            self.DrawPolygon(iw,4)
         return(self.__object, self.__doors, self.__excludeds, self.__cashdesks)  
 
-    def ReadStop(self, FileName):
-        file = open(FileName, 'r').read()
-        lines = file.split('\n')
-        points = []
-        i = 0
-        for line in lines:
-            if i%2 == 0:
-                line = line.split(',')
-                x = int(line[0])
-                y = int(line[1])
-            else:
-                line = line.split(' ')
-                probs = np.arange(int(line[0]),int(line[1]))
-                temp = random.sample(probs,1)
-            self.__grid[x][y].ChangeStops(temp)
-            i += 1
-            
+    #zwracanie rozmiaru pomieszczenia
+    def GetSize(self):
+        return self.__size
+    
+    #tworzenie odcieni szarości do zaznaczania stężenia   
     def Gray(self,im):
         im = 255 * (im/im.max())
         w,h = im.shape
@@ -151,6 +150,7 @@ class Object():
         ret[:,:,2] = ret[:,:,1] = ret[:,:,0] = im
         return ret
 
+    #tworzenie macierzy z szarościami odpowiadającymi stężeniom
     def ConcentrationVirusVisual(self,scale):
         dosc = np.zeros((self.__size[0]*scale,self.__size[1]*scale))
         next_x = 0
@@ -163,55 +163,104 @@ class Object():
             if i != 0 and i%scale == 0:
                 next_x += 1
             next_y = 0
-        dosc = 255 * dosc / dosc.max()
-        dosc = self.Gray(dosc)
-        return(dosc)
-          
-    def Motion(self, parent, steps, prob_in, max_humans, mask, vir,vir_d, vir_tr, stat_I, factor_mask, factor_I, max_prob_I):
-        pg.init()
-        scale = 50
-        screen = pg.display.set_mode((self.__size[0]*scale,self.__size[1]*scale))
+        matrix_color = 255 * dosc / dosc.max()
+        matrix_color = self.Gray(dosc)
+        return(matrix_color)
+
+    #wizualizacja człowieka
+    def VisualHuman(self,scale,screen,h,new_step):
+        #m = new_step.count(h.GetPosition())
+        if h.GetStatus() == 'I':
+            pg.draw.circle(screen, (255, 0, 0), (int(h.GetPosition()[0] * scale), int(h.GetPosition()[1] * scale)), scale/5)
+        else:
+            pg.draw.circle(screen, (100, 240, 50), (int(h.GetPosition()[0] * scale), int(h.GetPosition()[1] * scale)), scale/5)
+        pg.display.update()
+        
+    #wizualizacja macierzy z kolorami stężeń
+    def VisualConcentration(self,scale,screen):
+        matrix_color = self.ConcentrationVirusVisual(scale)
+        surf = pg.surfarray.make_surface(matrix_color)
+        screen.blit(surf,(0,0))
+        pg.display.update()
+
+    #wizualizacja tła, tj. scian, drzwi, recepcji, kas itp.
+    def VisualBackground(self,scale,screen):
         for ex in self.__excludeds:
             pg.draw.polygon(screen,(255,255,255), ((ex[0][0]*scale,ex[0][1]*scale),(ex[1][0]*scale,ex[1][1]*scale),(ex[2][0]*scale,ex[2][1]*scale),(ex[3][0]*scale,ex[3][1]*scale)),1)
         for d in self.__doors:
             pg.draw.line(screen,(0,0,255),(d[0][0]*scale,d[0][1]*scale),(d[1][0]*scale,d[1][1]*scale),20)
         for cd in self.__cashdesks:
-            pg.draw.polygon(screen,(255,0,255), ((cd[0][0]*scale,cd[0][1]*scale),(cd[1][0]*scale,cd[1][1]*scale),(cd[2][0]*scale,cd[2][1]*scale),(cd[3][0]*scale,cd[3][1]*scale)),1)
+            pg.draw.polygon(screen,(255,255,255), ((cd[0][0]*scale,cd[0][1]*scale),(cd[1][0]*scale,cd[1][1]*scale),(cd[2][0]*scale,cd[2][1]*scale),(cd[3][0]*scale,cd[3][1]*scale)),1)
         for a in self.__actives:
             pg.draw.polygon(screen,(255,255,0), ((a[0][0]*scale,a[0][1]*scale),(a[1][0]*scale,a[1][1]*scale),(a[2][0]*scale,a[2][1]*scale),(a[3][0]*scale,a[3][1]*scale)),1)
-        we = 0
-        wy = 0
-        I = 0
-        IIn = 0
-        number_step = []
-        stanI = []
-        I_temp = 0
+        for w in self.__inwalls:
+            pg.draw.line(screen,(255,255,255),(w[0][0]*scale,w[0][1]*scale),(w[1][0]*scale,w[1][1]*scale),3) 
+        for iD in self.__indoors:
+            pg.draw.line(screen,(0,0,255),(iD[0][0]*scale,iD[0][1]*scale),(iD[1][0]*scale,iD[1][1]*scale),3)
+        pg.display.update()
+
+    #ruch wirusa, jego przekazywanie do sąsiadów oraz umieranie
+    def MotionVirus(self,vir_d,vir_tr):
+        for m in range(self.__size[0]+1):
+            for n in range(self.__size[1]+1):
+                if self.__grid[m][n].GetVirus() > 0:
+                    new = (1 - vir_d/100) * self.__grid[m][n].GetVirus()
+                    temp = vir_tr/100 * new/8
+                    self.__grid[m][n].ChangeVirus(0.8 * new)
+                    mx = -1
+                    ny = -1
+                    for mm in range(2):
+                        for nn in range(2):
+                            if ny != 0 and mx != 0:
+                                if self.__grid[m+mx][n+ny].GetType() == 4:
+                                    self.__grid[m][n].AddVirus(temp)
+                                else:
+                                    try:
+                                        self.__grid[m+mx][n+ny].AddVirus(temp)
+                                    except:
+                                        self.__grid[m][n].AddVirus(temp)
+                            ny += 1
+                        mx += 1
+
+    #główna funkcja, tworzenie ludzi, ruch ludzi, wywołanie ruchu wirusa, wywołanie wizualizacji          
+    def Motion(self, parent, steps, prob_in, max_humans, mask, vir,vir_d, vir_tr, stat_I, factor_mask, factor_I, max_prob_I):
+        pg.init()
+        scale = 30 #powiększenie oknia wizualizacji
+        screen = pg.display.set_mode((self.__size[0]*scale,self.__size[1]*scale))
+        we = 0 #liczba osób, która weszła
+        wy = 0 #liczba osób, która wyszła
+        number_step = [] #lista z liczbą kroków
+        stanI = [] #lista z zarażonymi w danym kroku
+        I_temp = 0 #tymczasowa zmienna do zliczania zarażeń w danym kroku
         clock = pg.time.Clock()
         for i in range(steps):
             number_step.append(i+1)
-            x = random.random()
+            x = random.random() #losowanie z przedziału (0,1)
+            #ustalenie czy człowiek wchodzi w danym kroku
             if x <= prob_in/100 and self.__grid[int(parent.Start()[0])][int(parent.Start()[1])].GetHumans()<=max_humans:
+                #na podstawie prawdopodobieństwa podanego w argumencie Motion ustalanie statusu człowieka oraz czy dany człowiek posiada maseczkę
                 if x <= prob_in/100 * stat_I/100:
                     status = 'I'
-                    IIn += 1
                 else:
                     status = 'S'
                 if x <= (prob_in/100 * mask/100):
                     mask = True
                 else:
                     mask = False
-                if status == 'I' and mask == 'True': self.__grid[parent.Start()[0]][parent.Start()[1]].ChangeVirus(1/factor_mask)
-                elif status == 'I' and mask == 'False': self.__grid[parent.Start()[0]][parent.Start()[1]].ChangeVirus(1)
+                #w zależności od stanu i posiadania/braku maseczki wchodzącego człowieka ustalanie ile wirusa jest na wejściu
+                if status == 'I' and mask == 'True': self.__grid[parent.Start()[0]][parent.Start()[1]].ChangeVirus(vir/factor_mask)
+                elif status == 'I' and mask == 'False': self.__grid[parent.Start()[0]][parent.Start()[1]].ChangeVirus(vir)
+                #tworzenie człowieka
                 human = Human(parent.Start(), status, mask)
+                #dodawanie obiektu human do listy ze wszystkimi powstałymi obiektami 
                 self.__humans.append(human)
                 we += 1
             j = 0
-            positions_list = []
             new_step = []
-            max_queue = 1
+            max_queue = 0
+            #robienie kroku każdym człowiekiem, zmienianie stężenia wirusa w gridzie na podstawie statusu człowieka
             for h in self.__humans:
-                n = positions_list.count(h.GetPosition())
-                positions_list.append(h.GetPosition())
+                self.VisualHuman(scale,screen,h,new_step)
                 if h.GetPosition()[0] in self.__cashdesksinfY:
                     for i in range(len(self.__cashdesksinfY)):
                         if h.GetPosition()[0] == self.__cashdesksinfY[i] and h.GetPosition()[1] in range(self.__cashdesksinfX[i][0],self.__cashdesksinfX[i][1]+1):
@@ -233,76 +282,31 @@ class Object():
                         prob = (max_prob_I/100)/factor
                     if risk <= prob:
                         h.ChangeStatus('I')
-                        I += 1
                         I_temp += 1
                         if h.GetMask():
                             self.__grid[int(temp[0]+1)][int(temp[1]+1)].AddVirus(vir/factor_mask)
                         else:
                             self.__grid[int(temp[0]+1)][int(temp[1]+1)].AddVirus(vir)
-                m = new_step.count(h.GetPosition())
                 new_step.append(h.GetPosition())
-                if h.GetStatus() == 'I':
-                    pg.draw.circle(screen, (255, 0, 0), (h.GetPosition()[0] * scale, h.GetPosition()[1] * scale + m * 10), 5)
-                else:
-                    pg.draw.circle(screen, (100, 240, 50), (h.GetPosition()[0] * scale, h.GetPosition()[1] * scale + m * 10), 5)
+                #self.VisualHuman(scale,screen,h,new_step)
+                #pg.display.update()
+                #usuwanie człowieka z listy oraz obiektu gdy dojdzie do wyjścia
                 if temp == parent.End():
-                    del h
                     del self.__humans[j]
+                    del h
                     wy += 1
                     self.__grid[int(temp[0])][int(temp[1])].AddHuman(-1)
-                j += 1
-                pg.display.update()
+                else:
+                    j+=1
+                #pg.display.update()
             self.__wejscie.append(we)
             self.__wyjscie.append(wy)
-            for m in range(self.__size[0]+1):
-                for n in range(self.__size[1]+1):
-                    if self.__grid[m][n].GetVirus() > 0:
-                        new = (1 - vir_d/100) * self.__grid[m][n].GetVirus()
-                        temp = vir_tr/100 * new/8
-                        self.__grid[m][n].ChangeVirus(0.8 * new)
-                        mx = -1
-                        ny = -1
-                        for mm in range(2):
-                            for nn in range(2):
-                                if ny != 0 and mx != 0:
-                                    try:
-                                        self.__grid[m+mx][n+ny].AddVirus(temp)
-                                    except:
-                                        self.__grid[m][n].AddVirus(temp)
-                                    ny += 1
-                            mx += 1
-            dosc = self.ConcentrationVirusVisual(scale)
-            surf = pg.surfarray.make_surface(dosc)
-            screen.blit(surf,(0,0))
-            pg.display.update()
-            for ex in self.__excludeds:
-                pg.draw.polygon(screen,(255,255,255), ((ex[0][0]*scale,ex[0][1]*scale),(ex[1][0]*scale,ex[1][1]*scale),(ex[2][0]*scale,ex[2][1]*scale),(ex[3][0]*scale,ex[3][1]*scale)),1)
-            for d in self.__doors:
-                pg.draw.line(screen,(0,0,255),(d[0][0]*scale,d[0][1]*scale),(d[1][0]*scale,d[1][1]*scale),20)
-            for cd in self.__cashdesks:
-                pg.draw.polygon(screen,(255,255,255), ((cd[0][0]*scale,cd[0][1]*scale),(cd[1][0]*scale,cd[1][1]*scale),(cd[2][0]*scale,cd[2][1]*scale),(cd[3][0]*scale,cd[3][1]*scale)),1)
-            for a in self.__actives:
-                pg.draw.polygon(screen,(255,255,0), ((a[0][0]*scale,a[0][1]*scale),(a[1][0]*scale,a[1][1]*scale),(a[2][0]*scale,a[2][1]*scale),(a[3][0]*scale,a[3][1]*scale)),1)
-            pg.display.update()
+            self.MotionVirus(vir_d,vir_tr)
             stanI.append(I_temp)
-            demendedFps = 5
+            self.VisualConcentration(scale,screen)
+            self.VisualBackground(scale,screen)
+            #liczba klatek na sekundę - opóźnienie/przyspieszenie wizualizacji
+            demendedFps = 50
             clock.tick(demendedFps)
         pg.quit()
-        return(number_step,stanI)         
-
-    def Visual(self):
-        x = np.arange(len(self.__wejscie))
-        plt.plot(x,self.__wejscie)
-        plt.plot(x,self.__wyjscie)
-        plt.xlabel('krok')
-        plt.ylabel('liczba ludzi')
-        plt.legend(('wejscie', 'wyjscie'))
-        plt.show()
-
-    def ConcentrationVirus(self):
-        matrix = np.zeros((self.__size[0],self.__size[1]))
-        for i in range(self.__size[0]):
-            for j in range(self.__size[1]):
-                matrix[i][j] = self.__grid[i][j].GetVirus()
-        plt.imshow(matrix)
-        plt.show()
+        return(number_step,stanI)
